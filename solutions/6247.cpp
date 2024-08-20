06f280e3eb6f674deab805ae18f452df2b4fb464
Seungchan Lee <imchan0325@gmail.com>
2024-08-20 22:48:40 +0900

    Solved 6247

--- solutions/6247.cpp
+++ solutions/6247.cpp
@@ -2,46 +2,48 @@
 
 using namespace std;
 
-const int MAXN = 1e6 + 5;
+const int MAXN = 1000005;
+const int LOGN = 20;
 
-int N, K, indeg[MAXN], P[MAXN], root[MAXN], sz[MAXN];
+int N, K, P[MAXN], height[MAXN], leaf[MAXN];
+vector<int> leaves;
+vector<int> tree[MAXN];
 
-int find(int x)
+void dfs(int u)
 {
-    if(root[x] == x) return x;
-    return root[x] = find(root[x]);
-}
-
-void merge(int x, int y)
-{
-    x = find(x);
-    y = find(y);
-
-    if(x == y) return;
-
-    root[y] = x;
-    sz[x] += sz[y];
-}
-
-void mmeerrggee(int u)
-{
-    merge(P[u], u);
-    if(indeg[P[u]] == 1) mmeerrggee(P[u]);
+    if(tree[u].empty()) {
+        height[u] = 1;
+        leaf[u] = u;
+        leaves.push_back(u);
+        return;
+    }
+    for(auto v : tree[u]) {
+        dfs(v);
+        if(height[u] < height[v]) {
+            height[u] = height[v];
+            leaf[u] = leaf[v];
+        }
+    }
+    height[u]++;
+    height[leaf[u]]++;
 }
 
 int main()
 {
     cin >> N >> K;
+    for(int i = 1; i < N; i++) cin >> P[i];
     for(int i = 1; i < N; i++) {
-        cin >> P[i];
-        indeg[P[i]]++;
+        tree[P[i]].push_back(i);
     }
 
-    for(int i = 0; i < N; i++) {
-        if(indeg[i] == 0) {
-            mmeerrggee(i);
-        }
+    dfs(0);
+    sort(leaves.begin(), leaves.end(), [](int i, int j){return height[i] > height[j];});
+    int sum = 0;
+    int cnt = 0;
+    for(int i = 0; i < leaves.size(); i++) {
+        if(sum >= N-K) break;
+        sum += height[leaves[i]];
+        cnt++;
     }
-
-    
+    cout << cnt;
 }
